90e333954ede9d46e83e47593671ff07ad18a7a0
imasj-git <joshisaurav044@gamil.com>
2024-12-24 22:17:07 +0545

    created CRUD opertions for handling new item

---
+++ controller/BusController.js
@@ -0,0 +1,72 @@
+
+const Bus = require('../model/Bus')
+const findAll = async (req, res) => {
+    try {
+        const buses = await Bus.find().populate('routeid');
+        res.status(200).json(buses);
+    } catch (e) {
+        res.json(e)
+    }
+
+}
+const save = async (req, res) => {
+    try {
+        const { name, description, busnumber, busname, type, totalseat } = req.body
+        const bus = new Bus({
+            name,
+            description,
+            busnumber,
+            busname,
+            type,
+            totalseat,
+            image: req.file.originalname
+        });
+        await bus.save();
+        res.status(201).json(bus)
+    } catch (e) {
+        res.json(e)
+    }
+
+}
+const findById = async (req, res) => {
+    try {
+        const bus = await Bus.findById(req.params.id);
+        res.status(200).json(bus)
+    } catch (e) {
+        res.json(e)
+
+    }
+
+
+}
+const deleteById = async (req, res) => {
+    try {
+        const bus = await Bus.findByIdAndDelete(req.params.id);
+        res.status(200).json("data Deleted")
+    } catch (e) {
+        res.json(e)
+
+    }
+
+
+}
+const update = async (req, res) => {
+    try {
+        const bus = await Bus.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.status(201).json(bus)
+    } catch (e) {
+        res.json(e)
+
+    }
+
+
+}
+
+module.exports = {
+    findAll,
+    save,
+    findById,
+    deleteById,
+    update
+
+}
