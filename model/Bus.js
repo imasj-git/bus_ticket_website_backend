20e192f57088398504eae53d16f93228bf07038e
imasj-git <joshisaurav044@gamil.com>
2024-12-24 21:58:05 +0545

    created bus schema with attributes

---
+++ model/Bus.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+const busSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    busnumber: {
+        type: Number,
+        required: true
+    },
+    busname: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    totalseat: {
+        type: Number,
+        required: true
+    },
+    routeid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "routes",
+    }
+
+})
+const Bus = mongoose.model("buses", busSchema);
+
+module.exports = Bus;
