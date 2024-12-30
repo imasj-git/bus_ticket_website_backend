c286e98bed1a07672a42ea770960586aae0bd7bb
imasj-git <joshisaurav044@gamil.com>
2024-12-30 23:57:06 +0545

    customer route added

---
+++ routes/CustomerRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router();
+const { findAll, save, findById, deleteById, update } = require("../controller/CustomerController");
+const CustomerValidation = require("../validation/CustomerValidation");
+
+
+router.get("/", findAll);
+router.post("/", CustomerValidation, save);
+router.get("/:id", findById);
+router.delete("/:id", deleteById);
+router.put("/:id", update)
+
+
+
+module.exports = router;
